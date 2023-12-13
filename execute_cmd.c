2cfcc6f5433e200c1f340d9eb01a563c3b015074
BrahimElhajji <elhajji080898@gmail.com>
2023-12-13 15:47:31 -0800

    function that excute the command

---
+++ execute_cmd.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+/**
+ * execute_command - function that execute the commands
+ * @args: Argument Vector
+ * @c: an integer
+ * @envp: Environment Pointe
+ * Return: Success
+ */
+
+int execute_command(char **args, int c, char **envp)
+
+{
+	pid_t babyP;
+	int status;
+	char *cmd;
+
+
+	babyP = fork();
+
+	if (babyP == -1)
+	{
+		perror("fork");
+		return (EXIT_FAILURE);
+	}
+
+	if (babyP == 0)
+	{
+
+		cmd = ara_lcommand(args[0]);
+		if (cmd)
+			execve(cmd, args, envp);
+		else
+		{
+			error(args[0], c, envp);
+			khwi(args);
+			exit(127);
+		}
+	} else
+	{
+
+		waitpid(babyP, &status, 0);
+		khwi(args);
+	}
+	return (WEXITSTATUS(status));
+}
