baccecb6f6d0a7c25d39f1dc824aa428ab2ad8b1
BrahimElhajji <elhajji080898@gmail.com>
2023-12-13 15:21:38 -0800

    updated shell.h

--- shell.h
+++ shell.h
@@ -10,28 +10,24 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
-#define MAX_TOKENS 128
 #define DELIMITER " \t\r\n\a"
 extern char **environ;
-#define MAX_ARGS 1024
 
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+int _strlen(const char *s);
+char *_strdup(char *src);
 int _strncmp( const char * s1, const char * s2, size_t n );
+
 char * read_user_input();
 void display_prompt();
 char ** tokenize_string(char *input_line);
-int execute_command(char **args, char **envp, int i);
-void freearray2D(char **arr);
-
+int execute_command(char **args, int c, char **envp);
+void khwi(char **massfoufa);
 char *_getenv(const char *env_var);
-void printing_error(char *nm, char *command, int i);
-char *get_command(char *command);
-char *my_itoa(int n);
-void rev_string(char *s, int len);
-
-
+char *ara_lcommand(char *amr);
+char *me_itoa(unsigned int n);
+void error(char *key, int chip, char **argv);
+void print_f(const char *s);
 
 #endif
-
