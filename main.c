4ee150d5eb587d37506a81e47a69e10776628fc1
ismailouzy <ismailouzal@gmail.com>
2023-12-13 15:30:58 -0800

    main

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+/**
+ * main - entry point
+ * @argc: argc parametre
+ * @argv: argv paramtre
+ * Return: Success
+ */
+
+int main(int argc, char **argv)
+{
+	char *user_input, **command = NULL;
+
+	int status = 0, c = 0;
+	(void)argc;
+
+
+	while (1)
+	{
+		user_input = read_user_input();
+
+
+		if (user_input != NULL)
+		{
+
+			command = tokenize_string(user_input);
+			if (!command)
+				continue;
+
+
+			status = execute_command(command, c, argv);
+
+			free(user_input);
+		} else
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			return (status);
+		}
+
+		c++;
+
+	}
+	return (0);
+}
